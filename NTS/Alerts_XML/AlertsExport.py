918ed494ccb44afebdac0fc98f966c5b5b15feed
yoshisatoh <73818039+yoshisatoh@users.noreply.github.com>
2021-09-03 19:02:56 +0900

    Add files via upload

--- NTS/Alerts_XML/AlertsExport.py
+++ NTS/Alerts_XML/AlertsExport.py
@@ -3,7 +3,7 @@
 #  All rights reserved.
 #
 # Created:      2021/08/26
-# Last Updated: 2021/09/02
+# Last Updated: 2021/09/03
 #
 # Github:
 # https://github.com/yoshisatoh/nasdaq/tree/main/NTS/Alerts_XML/AlertsExport.py
@@ -12,7 +12,7 @@
 # Notes:
 #
 # First off, download an AlertsExport.zip file from a Nasdaq Trade Surveillance website for your company.
-# For example, a zip file can be downloaded by using the following URL.
+# For example, you can downloade a zip file if you replace <yourorganisation> with your company's specific ID.
 # https://<yourorganisation>.smartsbroker.com/cmss/citadel/exportAlerts?marketCode=asx&date=20210730&bundle=true&apiVersion=8&lookbackDays=6
 #
 # Second, unzip the AlertsExport.zip file on your computer. An AlertsExport directory will be created.
@@ -22,20 +22,27 @@
 # 
 # Finally, execute this script on your Command Prompt (Windows) or Terminal (MacOS) as follows.
 # python AlertsExport.py
+# (Your environment might need python3 instead of python.)
 #
-# If it is successful, the following files will be created:
-# (market).(yyyymmdd).alerts.csv    -    Details of alerts for each day
+# If it is successful, then following files will be created:
+#
+# (market).(yyyymmdd).alerts.csv    -    Details of alerts for each trading/business day
 # (market).all.alerts.csv    -    An aggregated file of all the (market).(yyyymmdd).alerts.csv files
-# (market).all.alerts.counts.csv    -    Aggregated alert counts for each alert during a specified period of time
-# (market).png    -    a bar chart (x: date, y: total alertCount per day)
-# summary.markets.csv    -    (market), (yyyymmdd), (total alertCount per day)
+# (market).all.alerts.counts.csv    -    Aggregated alert counts for each alert during a specified time period
+# (market).png    -    a bar chart (x: date, y: total alert counts per day)
+# (market).all.alerts.counts.house.csv    -    Aggregated alert counts during a specified time period for certain columns: house
+# (market).all.alerts.counts.house.account_ref.csv    -    Aggregated alert counts during a specified time period for certain columns: house, account_ref
+# (market).all.alerts.counts.house.security.csv    -    Aggregated alert counts during a specified time period for certain columns: house, security
+# (market).all.alerts.counts.house.trader.csv    -    Aggregated alert counts during a specified time period for certain columns: house, trader
+# (market).all.alerts.counts.house.trader.account_ref.security.csv    -    Aggregated alert counts during a specified time period for certain columns: house, trader, account_ref, security
+# summary.markets.csv    -    (market), (yyyymmdd), (total alert counts per calendar day)
 # summary.markets.market.csv    -    (market)
-# summary.total.csv    -    (total alertCount for a specific period of time - if you use an example URL above, that covers from 24th July 2021 to 30th July 2021, i.e., 7 = 6+1 calendar days)
+# summary.total.csv    -    (total alert counts for a specific period of time - if you use an example URL above, that covers from 24th July 2021 to 30th July 2021, i.e., 7 = 6+1 calendar days)
 
 
 
 
-#################### import library ####################
+#################### import libraries ####################
 import xml.etree.ElementTree as ET
 import csv
 import matplotlib.pyplot as plt
@@ -45,6 +52,7 @@ import numpy as np
 import sys
 import glob
 import os
+import    pathlib
 
 
 
@@ -52,17 +60,16 @@ import os
 #################### AlertsExport/summary.xml ####################
 
 
-########## Loading summary.xml
+########## Load summary.xml
 
-# Load summary.xml file and treat as "tree"
+# Load a summary.xml file for a specified time period, and treat as "tree"
 tree = ET.parse('AlertsExport/summary.xml')
 
 # Pull the root (topmost) element of the tree
 root = tree.getroot()
 
 
-########## Creating summary.total.csv (total alert count)
-# Create summary.total.csv file for: alertCount (total alert count for a specified period)
+########## Create summary.total.csv
 with open('summary.total.csv', 'w', newline='') as f:
     writer = csv.writer(f)
     writer.writerow([root.find('total/alertCount').text])
@@ -70,12 +77,9 @@ with open('summary.total.csv', 'w', newline='') as f:
 #
 #print(f.closed)
 #True
-#
-#print(total_alertCount)
 
 
-########## Creating summary.markets.csv (market, yyyymmdd, daily alert count)
-# Create summary.markets.csv file with three columns: marketCode, date, alertCount
+########## Create summary.markets.csv
 with open('summary.markets.csv', 'w', newline='') as f:
     writer = csv.writer(f)
     #
@@ -119,37 +123,28 @@ df1 = pd.DataFrame(summarydt)
 
 
 
-########## Creating summary.markets.market.csv (market)
+
+########## Create summary.markets.market.csv
+#
 # Extract all the markets
 df2 = pd.DataFrame(df1.iloc[:, 0].unique())
-#print(df2)
-#print(type(df2))
 #
 df2.to_csv('summary.markets.market.csv', header=False, index=False)
 ls2 = list(pd.read_csv('summary.markets.market.csv'))
 #
-#print(ls2)
-#['(market)']
-#
-#print(len(ls2))
 #
 ln2 = len(ls2)
-print(ln2)
-#1
-#
-#print(ls2[0])
-#'(market)'
-#
-#print(type(ls2[0]))
-#<class 'str'>
 
 
-########## Creating (market).png (x: day, y: daily alert count)
+
+
+########## Create (market).png (x: day, y: daily alert count)
+#
 # Draw a bar chart
 #
 for n in range(ln2):
     #
-    print(df1[df1['market'] == ls2[n]])
+    #print(df1[df1['market'] == ls2[n]])
     #
     X = list(df1['date'])
     X = pd.to_datetime(X, format='%Y%m%d')
@@ -163,7 +158,7 @@ for n in range(ln2):
     plt.xlabel("date")
     plt.ylabel("alertCount")
     #
-    # Save the plot
+    # Save the plot as a png file
     plt.savefig(ls2[n] + '.png')
     #
     # Show the plot
@@ -175,25 +170,18 @@ for n in range(ln2):
 
 #################### AlertsExport/(market)/(yyyymmdd)/alerts.xml ####################
 
-#print(ls2[0])
-#'(market)'
-
 mkt = str(ls2[0])
-#print(mkt)
 
-#print(df1)
-#print(type(df1))
-#
-#Delete dates with no alert
+# Delete dates with no alert, which are usually non-trading/business days
 df1nz = df1[df1['alertCount'] != 0]
-#
+
 datelst = list(df1nz['date'])
 
 
 ########## Creating (market).(yyyymmdd).alerts.csv (alert id, market, date, alertTime, alert title)
 for n in range(len(datelst)):
     #
-    # Load alerts.xml file and treat as "tree"
+    # Load alerts.xml and treat as "tree"
     tree = ET.parse('AlertsExport/' + mkt + '/' + str(datelst[n]) + '/alerts.xml')
     #
     # Pull the root (topmost) element of the tree
@@ -204,7 +192,6 @@ for n in range(len(datelst)):
         writer = csv.writer(f)
         #
         #####
-        # writer.writerow([dir1.attrib['id'], dir1.attrib['id'].split('-')[0], datetime.datetime.strptime(dir1.attrib['id'].split('-')[1], '%Y%m%d').date(), dir1.attrib['id'].split('-')[2], dir1.find("alertTime").text.split('T')[1].split('+')[0], dir1.find("title").text, dir1.find("sources").find("source").find("target").find("primaryTarget").find("security").text])
         #
         # (column name):     (xml)
         # alert_id:          dir1.attrib['id']
@@ -218,7 +205,7 @@ for n in range(len(datelst)):
         # partcipants>trader: dir1.find("participants").find("trader").text
         # partcipants>account>ref: dir1.find("participants").find("account").find("ref").text
         #
-        ##### adding column names
+        # add column names
         writer.writerow(["alert_id", "market", "date", "alertCount", "time", "title", "security", "house", "trader", "account_ref"])
         #####
         #
@@ -229,24 +216,23 @@ for n in range(len(datelst)):
             numofalert = numofalert + 1
             #
             #####
-            # The following alerts are skipped as it does not have security code:
+            # The following alerts are skipped as it does NOT have security code:
             #
             # NEW PRICE HIGH ON LAST TRADING DAY OF MONTH SUMMARY (HOUSE)
             # AFTER PROCESSING COMPLETE (HOUSE)
+            #
             #####
             if dir1.find("title").text == "NEW PRICE HIGH ON LAST TRADING DAY OF MONTH SUMMARY (HOUSE)":
                 #break
                 continue
+                #
             elif  dir1.find("title").text == "AFTER PROCESSING COMPLETE (HOUSE)":
                 #break
                 continue
+                #
             else:
                 #
-                #writer.writerow([dir1.attrib['id'], dir1.attrib['id'].split('-')[0], datetime.datetime.strptime(dir1.attrib['id'].split('-')[1], '%Y%m%d').date(), dir1.attrib['id'].split('-')[2], dir1.find("alertTime").text.split('T')[1].split('+')[0], dir1.find("title").text, dir1.find("sources").find("source").find("target").find("primaryTarget").find("security").text, dir1.find("participants").find("house").text])
-                #
-                #print(dir1.find("participants").find("house") is None)
-                #print(dir1.find("participants").find("trader") is None)
-                #print(dir1.find("participants").find("account") is None)
+                # 1. both trader and account/ref are available
                 #
                 if (not(dir1.find("participants").find("trader") is None) and not(dir1.find("participants").find("account") is None)):
                     #
@@ -260,8 +246,9 @@ for n in range(len(datelst)):
                                      dir1.find("participants").find("trader").text,
                                      dir1.find("participants").find("account").find("ref").text])
                     #
-                    #print(dir1.find("participants").find("trader").text)
-                    #
+                #
+                # 2. trader is available and account is NOT available
+                #
                 elif (not(dir1.find("participants").find("trader") is None) and (dir1.find("participants").find("account") is None)):
                     #
                     writer.writerow([dir1.attrib['id'],
@@ -274,6 +261,9 @@ for n in range(len(datelst)):
                                      dir1.find("participants").find("trader").text,
                                      'NA'])
                     #
+                #
+                # 3. trader is NOT available and account is available
+                #
                 elif ((dir1.find("participants").find("trader") is None) and not(dir1.find("participants").find("account") is None)):
                     #
                     writer.writerow([dir1.attrib['id'],
@@ -286,6 +276,9 @@ for n in range(len(datelst)):
                                      'NA',
                                      dir1.find("participants").find("account").find("ref").text])
                     #
+                #
+                # 4. NEITHER trader nor account/ref is available
+                #
                 else:
                     #
                     writer.writerow([dir1.attrib['id'],
@@ -299,14 +292,6 @@ for n in range(len(datelst)):
                                      'NA'])
                     #
                 #
-                #print(dir1.find("participants").find("house").text) if (dir1.find("participants").find("house").text) else print('NA')
-                #print(dir1.find("participants").find("trader").text) if (dir1.find("participants").find("trader")) else print('NA')
-                #print(dir1.find("participants").find("account").find("ref").text) if (dir1.find("participants").find("account")) else print('NA')
-                #
-                #if dir1.find("participants").find("house").text:
-                #    print(dir1.find("participants").find("house").text)
-                #else:
-                #    print("NA")
                 #####
             #
 #
@@ -317,15 +302,19 @@ for n in range(len(datelst)):
 
 
 
-#################### Merging all the (market).(yyyymmdd).alerts.csv files into (market).all.alerts.csv, and then create (market).all.alerts.counts.csv ####################
+#################### Merge all the (market).(yyyymmdd).alerts.csv files into (market).all.alerts.csv, and then create (market).all.alerts.counts.csv ####################
 
-#Deleting an old file: (market).all.alerts.csv
+# Create an empty file, or overwrite if it already exists: (market).all.alerts.csv
+empty_file = pathlib.Path(mkt + '.' + 'all' + '.alerts.csv')
+empty_file.touch()
+#
+# Delete an old file if it exists: (market).all.alerts.csv
 os.remove(mkt + '.' + 'all' + '.alerts.csv')
 
-#Load all the files: (market).(yyyymmdd).alerts.csv
+# Load all the files: (market).(yyyymmdd).alerts.csv
 files = glob.glob(mkt + '.' + '*' + '.alerts.csv')
 
-# Create (market).all.alerts.csv file
+# Create (market).all.alerts.csv
 with open(mkt + '.' + 'all' + '.alerts.csv', 'w', newline='') as f_new:
     #
     ##### adding column names
@@ -336,7 +325,7 @@ with open(mkt + '.' + 'all' + '.alerts.csv', 'w', newline='') as f_new:
     for f in files:
         with open(f, 'r', newline='') as f_org:
             #
-            #Skipping header (first row)
+            # Skip header (first row)
             header = next(f_org)
             #
             f_new.write(f_org.read())
@@ -346,28 +335,61 @@ with open(mkt + '.' + 'all' + '.alerts.csv', 'w', newline='') as f_new:
 #
 
 
-# Sorting (mkt).all.alerts.csv by using the keys "date" and "alertCount"
+# Sort (mkt).all.alerts.csv by the keys "date" and "alertCount" in ascending order
 #
 mkt_all_alerts = pd.read_csv(mkt + '.' + 'all' + '.alerts.csv')
 #
-#print(mkt_all_alerts)
-#print(mkt_all_alerts.sort_values(by=["date", "alertCount"], ascending=[True, True]))
-#
 mkt_all_alerts = mkt_all_alerts.sort_values(by=["date", "alertCount"], ascending=[True, True])
+#
+# Overwrite (mkt).all.alerts.csv
 mkt_all_alerts.to_csv(mkt + '.' + 'all' + '.alerts.csv', index=False)
 
 
-# Creating (mkt).all.alerts.counts.csv - aggregated alert counts for each alert during a specified period of time
+# Create (mkt).all.alerts.counts.csv
 #
-#print(mkt_all_alerts['title'].value_counts())
-#print(type(mkt_all_alerts['title'].value_counts()))
+mkt_all_alerts_counts = pd.DataFrame(mkt_all_alerts['title'].value_counts()).rename(columns={'title': 'alerts'})
 #
-mkt_all_alerts['title'].value_counts().to_csv(mkt + '.' + 'all' + '.alerts.counts.csv', header=True, index=True)
+mkt_all_alerts_counts.to_csv(mkt + '.' + 'all' + '.alerts.counts.csv', header=True, index=True)
 #
 mkt_all_alerts_counts = pd.read_csv(mkt + '.' + 'all' + '.alerts.counts.csv')
+
+
+
+
+#################### Refer to (market).all.alerts.csv, calculate aggregated alert counts for each set of columns, and then create csv files for each
+
+
+##### (mkt).all.alerts.counts.house.csv
 #
-#print(mkt_all_alerts_counts)
-#print(pd.DataFrame(mkt_all_alerts_counts))
-#print(pd.DataFrame(mkt_all_alerts_counts).iloc[:,0])
-#print(pd.DataFrame(mkt_all_alerts_counts).iloc[:,1])
+mkt_all_alerts_house = pd.DataFrame(mkt_all_alerts.groupby(['house']).size())
+mkt_all_alerts_house.rename(columns={0: 'alerts'}, inplace=True)
+mkt_all_alerts_house.to_csv(mkt + '.' + 'all' + '.alerts.counts.house.csv', header=True, index=True)
+
+
+##### (mkt).all.alerts.counts.house.security.csv
+#
+mkt_all_alerts_house_security = pd.DataFrame(mkt_all_alerts.groupby(['house', 'security']).size())
+mkt_all_alerts_house_security.rename(columns={0: 'alerts'}, inplace=True)
+mkt_all_alerts_house_security.to_csv(mkt + '.' + 'all' + '.alerts.counts.house.security.csv', header=True, index=True)
+
+
+##### (mkt).all.alerts.counts.house.trader.csv
+#
+mkt_all_alerts_house_trader = pd.DataFrame(mkt_all_alerts.groupby(['house', 'trader']).size())
+mkt_all_alerts_house_trader.rename(columns={0: 'alerts'}, inplace=True)
+mkt_all_alerts_house_trader.to_csv(mkt + '.' + 'all' + '.alerts.counts.house.trader.csv', header=True, index=True)
+
+
+##### (mkt).all.alerts.counts.house.account_ref.csv
+#
+mkt_all_alerts_house_account_ref = pd.DataFrame(mkt_all_alerts.groupby(['house', 'account_ref']).size())
+mkt_all_alerts_house_account_ref.rename(columns={0: 'alerts'}, inplace=True)
+mkt_all_alerts_house_account_ref.to_csv(mkt + '.' + 'all' + '.alerts.counts.house.account_ref.csv', header=True, index=True)
+
+
+##### (mkt).all.alerts.counts.house.trader.account_ref.security.csv
+#
+mkt_all_alerts_house_trader_account_ref_security = pd.DataFrame(mkt_all_alerts.groupby(['house', 'trader', 'account_ref','security']).size())
+mkt_all_alerts_house_trader_account_ref_security.rename(columns={0: 'alerts'}, inplace=True)
+mkt_all_alerts_house_trader_account_ref_security.to_csv(mkt + '.' + 'all' + '.alerts.counts.house.trader.account_ref.security.csv', header=True, index=True)
 
